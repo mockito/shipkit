26679230311bbf8e4419150d708b89e3a1f650e8
Szczepan Faber <szczepiq@gmail.com>
2017-02-11 23:09:02 -0800

    Fixed the test

--- src/test/groovy/org/mockito/release/notes/improvements/GitHubTicketFetcherTest.groovy
+++ src/test/groovy/org/mockito/release/notes/improvements/GitHubTicketFetcherTest.groovy
@@ -14,13 +14,13 @@ class GitHubTicketFetcherTest extends Specification {
     def "fetches improvements from GitHub"() {
         def readOnlyToken = "a0a4c0f41c200f7c653323014d6a72a127764e17"
         when:
-        def improvements = fetcher.fetchTickets(readOnlyToken, ['109', '108', '99999', '112'], []) as List
+        def improvements = fetcher.fetchTickets(readOnlyToken, ['109', '108', '99999', '112'], [], false) as List
 
         then:
         //TODO SF we can leave this test but we should create a sample project for it instead of using Mockito repo
         //This way it will be faster, mockito repo has many issues and pagination takes a lot of time
         improvements[0].labels == ["enhancement"] as Set
-        new DefaultFormatter().format([:], improvements) == """* Improvements: 3
+        new DefaultFormatter([:]).format([:], improvements) == """* Improvements: 3
   * Allow instances of other classes in AdditionalAnswers.delegatesTo [(#112)](https://github.com/mockito/mockito/issues/112)
   * Improve automated release notes look [(#109)](https://github.com/mockito/mockito/issues/109)
   * Clarify Spy vs Mock CALLS_REAL_METHODS [(#108)](https://github.com/mockito/mockito/issues/108)"""
