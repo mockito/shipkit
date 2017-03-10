a3730a4e5b4b3fdc37eafe35d757e6e1d77c870f
Szczepan Faber <szczepiq@gmail.com>
2017-03-09 21:26:26 -0800

    Versioning plugin configures project's version

--- src/main/groovy/org/mockito/release/gradle/notes/VersioningPlugin.java
+++ src/main/groovy/org/mockito/release/gradle/notes/VersioningPlugin.java
@@ -7,8 +7,10 @@ import org.gradle.api.Project;
  * The plugin adds following tasks:
  *
  * <ul>
- *     <li>bumpVersionFile - increments version file, see {@link BumpVersionFileTask}</li>
+ *     <li>bumpVersionFile - increments version in "version.properties" file, see {@link BumpVersionFileTask}</li>
  * </ul>
+ *
+ * Also, the plugin configures project's version to the value specified in "version.properties"
  */
 public interface VersioningPlugin extends Plugin<Project> {
 }
--- src/main/groovy/org/mockito/release/internal/gradle/DefaultVersioningPlugin.java
+++ src/main/groovy/org/mockito/release/internal/gradle/DefaultVersioningPlugin.java
@@ -3,14 +3,21 @@ package org.mockito.release.internal.gradle;
 import org.gradle.api.Project;
 import org.mockito.release.gradle.notes.BumpVersionFileTask;
 import org.mockito.release.gradle.notes.VersioningPlugin;
+import org.mockito.release.version.Version;
+
+import java.io.File;
 
 import static org.mockito.release.internal.gradle.CommonSettings.TASK_GROUP;
 
 public class DefaultVersioningPlugin implements VersioningPlugin {
 
     public void apply(Project project) {
+        File versionFile = project.file("version.properties");
+
+        project.setVersion(Version.versionFile(versionFile).getVersion());
+
         BumpVersionFileTask task = project.getTasks().create("bumpVersionFile", DefaultBumpVersionFileTask.class);
-        task.setVersionFile(project.file("version.properties"));
+        task.setVersionFile(versionFile);
         task.setDescription("Increments version number in the properties file that contains the version.");
         task.setGroup(TASK_GROUP);
     }
