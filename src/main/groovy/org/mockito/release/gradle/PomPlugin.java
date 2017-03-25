f0303795261e8187d6b51426257b7e22ec978116
Szczepan Faber <szczepiq@gmail.com>
2017-03-24 18:28:13 -0700

    Added missing class in the interface signature
    We need those interfaces to extend Gradle types. This way, they can participate in the plugins.withType() and tasks.withType() mechanisms of Gradle.

--- src/main/groovy/org/mockito/release/gradle/PomPlugin.java
+++ src/main/groovy/org/mockito/release/gradle/PomPlugin.java
@@ -1,5 +1,8 @@
 package org.mockito.release.gradle;
 
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
 /**
  * Adds "ext.pom_customizePom(Project, MavenPublication)" method to the project.
  * The method can be used to customize the pom.
@@ -13,5 +16,5 @@ package org.mockito.release.gradle;
  *  <li> project.rootProject.ext.pom_contributors
  * </ul>
  */
-public interface PomPlugin {
+public interface PomPlugin extends Plugin<Project> {
 }
