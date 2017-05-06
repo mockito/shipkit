eb6537c985ea749ea8d0f3527657ceb9e47ebdbc
Szczepan Faber <szczepiq@gmail.com>
2017-05-06 14:05:17 -0700

    Added handy spec
    I will need this soon.

--- src/main/groovy/org/mockito/release/internal/gradle/util/Specs.java
+++ src/main/groovy/org/mockito/release/internal/gradle/util/Specs.java
@@ -3,6 +3,8 @@ package org.mockito.release.internal.gradle.util;
 import org.gradle.api.Task;
 import org.gradle.api.specs.Spec;
 
+import java.io.File;
+
 /**
  * Helper methods for Gradle Spec objects
  */
@@ -16,6 +18,18 @@ public class Specs {
             @Override
             public boolean isSatisfiedBy(Task t) {
                 return t.getName().equals(name);
+            }
+        };
+    }
+
+    /**
+     * Spec that checks if file exists using {@link File#isFile()} method.
+     */
+    public static Spec<Task> fileExists(final File file) {
+        return new Spec<Task>() {
+            @Override
+            public boolean isSatisfiedBy(Task task) {
+                return file.isFile();
             }
         };
     }
