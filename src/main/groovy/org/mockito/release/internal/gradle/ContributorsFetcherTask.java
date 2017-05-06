9ae31c98d8cfaba280ff0826eafd3adbcc88ba2f
Szczepan Faber <szczepiq@gmail.com>
2017-05-06 12:24:39 -0700

    Made fetching contributors incremental
    Also simplified the code

--- src/main/groovy/org/mockito/release/internal/gradle/ContributorsFetcherTask.java
+++ src/main/groovy/org/mockito/release/internal/gradle/ContributorsFetcherTask.java
@@ -4,20 +4,25 @@ import org.gradle.api.DefaultTask;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
 import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 import org.mockito.release.exec.Exec;
 import org.mockito.release.exec.ProcessRunner;
+import org.mockito.release.internal.gradle.util.ReleaseNotesSerializer;
 import org.mockito.release.notes.contributors.Contributors;
 import org.mockito.release.notes.contributors.ContributorsSerializer;
 import org.mockito.release.notes.contributors.ContributorsSet;
 import org.mockito.release.notes.contributors.GitHubContributorsProvider;
-import org.mockito.release.notes.model.ContributionSet;
-import org.mockito.release.notes.vcs.ContributionsProvider;
+import org.mockito.release.notes.model.Contribution;
+import org.mockito.release.notes.model.ReleaseNotesData;
 import org.mockito.release.notes.vcs.RevisionProvider;
 import org.mockito.release.notes.vcs.Vcs;
 
 import java.io.File;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Fetch info about contributors from GitHub and store it in file. It is used later in generation release notes.
@@ -30,24 +35,52 @@ public class ContributorsFetcherTask extends DefaultTask {
     @Input private String readOnlyAuthToken;
     @Input private String fromRevision;
     @Input private String toRevision;
+    @InputFile private File releaseNotesData;
 
     @OutputFile private File contributorsFile;
+
+    /**
+     * Data file containing serialized {@link ReleaseNotesData}.
+     */
+    public File getReleaseNotesData() {
+        return releaseNotesData;
+    }
+
+    /**
+     * See {@link #getReleaseNotesData()}
+     */
+    public void setReleaseNotesData(File releaseNotesData) {
+        this.releaseNotesData = releaseNotesData;
+    }
 
     @TaskAction
     public void fetchLastContributorsFromGitHub() {
         LOG.lifecycle("  Fetching contributors information between revisions {}..{}", fromRevision, toRevision);
         ProcessRunner processRunner = Exec.getProcessRunner(getProject().getRootDir());
-        ContributionsProvider contributionsProvider = Vcs.getContributionsProvider(processRunner);
-        ContributionSet contributions = contributionsProvider.getContributionsBetween(fromRevision, toRevision);
 
         RevisionProvider revisionProvider = Vcs.getRevisionProvider(processRunner);
+        //GitHub commits API does not give us tag names, it only operates on the SHA
+        //Therefore we need to map tag to SHA
         String fromRev = revisionProvider.getRevisionForTagOrRevision(fromRevision);
 
+        Collection<ReleaseNotesData> data = new ReleaseNotesSerializer(releaseNotesData).deserialize();
+        Collection<String> authorNames = getAuthorNames(data);
+
         GitHubContributorsProvider contributorsProvider = Contributors.getGitHubContributorsProvider(repository, readOnlyAuthToken);
-        ContributorsSet contributors = contributorsProvider.mapContributorsToGitHubUser(contributions, fromRev, toRevision);
+        ContributorsSet contributors = contributorsProvider.mapContributorsToGitHubUser(authorNames, fromRev, toRevision);
 
         ContributorsSerializer contributorsSerializer = Contributors.getLastContributorsSerializer(contributorsFile);
         contributorsSerializer.serialize(contributors);
+    }
+
+    private static Collection<String> getAuthorNames(Collection<ReleaseNotesData> data) {
+        Set<String> authors = new HashSet<String>();
+        for (ReleaseNotesData d : data) {
+            for (Contribution c : d.getContributions().getContributions()) {
+                authors.add(c.getAuthorName());
+            }
+        }
+        return authors;
     }
 
     public void setRepository(String repository) {
@@ -68,5 +101,25 @@ public class ContributorsFetcherTask extends DefaultTask {
 
     public void setContributorsFile(File contributorsFile) {
         this.contributorsFile = contributorsFile;
+    }
+
+    public String getRepository() {
+        return repository;
+    }
+
+    public String getReadOnlyAuthToken() {
+        return readOnlyAuthToken;
+    }
+
+    public String getFromRevision() {
+        return fromRevision;
+    }
+
+    public String getToRevision() {
+        return toRevision;
+    }
+
+    public File getContributorsFile() {
+        return contributorsFile;
     }
 }
--- src/main/groovy/org/mockito/release/notes/contributors/GitHubLastContributorsFetcher.java
+++ src/main/groovy/org/mockito/release/notes/contributors/GitHubLastContributorsFetcher.java
@@ -4,23 +4,25 @@ import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
 import org.json.simple.DeserializationException;
 import org.json.simple.JsonObject;
-import org.mockito.release.notes.model.Commit;
-import org.mockito.release.notes.model.Contribution;
+import org.mockito.release.internal.gradle.util.StringUtil;
 import org.mockito.release.notes.model.Contributor;
 import org.mockito.release.notes.util.GitHubListFetcher;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class GitHubLastContributorsFetcher {
 
     private static final Logger LOG = Logging.getLogger(GitHubLastContributorsFetcher.class);
 
-    ContributorsSet fetchContributors(String repository, String readOnlyAuthToken, Collection<Contribution> contributions, String fromRevision, String toRevision) {
+    ContributorsSet fetchContributors(String repository, String readOnlyAuthToken, Collection<String> authorNames, String fromRevision, String toRevision) {
         LOG.info("Querying GitHub API for commits (for contributors)");
         ContributorsSet result = new DefaultContributorsSet();
 
-        Set<String> authors = getAuthors(contributions);
+        Set<String> remaining = new HashSet<String>(authorNames);
 
         try {
             GitHubCommits commits = GitHubCommits.authenticatingWith(repository, readOnlyAuthToken)
@@ -28,15 +30,15 @@ public class GitHubLastContributorsFetcher {
                     .toRevision(toRevision)
                     .build();
 
-            while(!authors.isEmpty() && commits.hasNextPage()) {
+            while(!remaining.isEmpty() && commits.hasNextPage()) {
                 List<JsonObject> page = commits.nextPage();
-                result.addAllContributors(extractContributors(page, authors));
+                result.addAllContributors(extractContributors(page, remaining));
             }
-            if(!authors.isEmpty()) {
-                for (String author : authors) {
-                    //TODO SF Maybe use warn or error level here?
-                    LOG.info(cantFindProfileForAuthorError(author, contributions));
-                }
+            if (!remaining.isEmpty()) {
+                LOG.lifecycle("{} contributor(s) will not have link to GitHub profile in release notes data.\n" +
+                                "  We were not able to extract contributor information from GitHub API using 'commits' endpoint.\n" +
+                                "  Contributors with no profile: \n  {}",
+                        remaining.size(), StringUtil.join(remaining, ", "));
             }
         } catch (Exception e) {
             LOG.info("Problems fetching commits from GitHub", e);
@@ -44,14 +46,6 @@ public class GitHubLastContributorsFetcher {
         }
 
         return result;
-    }
-
-    private Set<String> getAuthors(Collection<Contribution> contributions) {
-        Set<String> authors = new HashSet<String>();
-        for (Contribution contribution : contributions) {
-            authors.add(contribution.getAuthorName());
-        }
-        return authors;
     }
 
     private Set<Contributor> extractContributors(List<JsonObject> commits, Set<String> authors) {
@@ -69,44 +63,6 @@ public class GitHubLastContributorsFetcher {
             }
         }
         return result;
-    }
-
-    private String cantFindProfileForAuthorError(String author, Collection<Contribution> contributions) {
-        Collection<Commit> commits = getCommitsForAuthors(contributions, author);
-        return "Can't find Profile for " + author + " commits: " + format(commits);
-    }
-
-    private String format(Collection<Commit> commits) {
-        Set<String> ids = new HashSet<String>();
-        for (Commit commit : commits) {
-            ids.add(commit.getCommitId());
-        }
-        return join(", ", ids);
-    }
-
-    private String join(String delimiter, Set<String> ids) {
-        String text = "";
-        for (String id : ids) {
-            text = text + id + delimiter;
-        }
-        return removeLastDelimiterIfExist(delimiter, text);
-    }
-
-    private String removeLastDelimiterIfExist(String delimiter, String text) {
-        if(text.length() > delimiter.length()) {
-            text = text.substring(0, text.length() - delimiter.length());
-        }
-        return text;
-    }
-
-    private Collection<Commit> getCommitsForAuthors(Collection<Contribution> contributions, String author) {
-        Set<Commit> commits = new HashSet<Commit>();
-        for (Contribution contribution : contributions) {
-            if(contribution.getAuthorName().equals(author)) {
-                commits.addAll(contribution.getCommits());
-            }
-        }
-        return commits;
     }
 
     private static class GitHubCommits {
