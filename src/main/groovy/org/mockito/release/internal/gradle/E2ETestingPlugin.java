0309e5ea29da0f43daddb625d85da31f4b56e254
Marcin Stachniuk <mstachniuk@gmail.com>
2017-05-14 14:44:22 +0200

    Continue work on e2e, but need fix issue 139 first

--- src/main/groovy/org/mockito/release/internal/gradle/E2ETestingPlugin.java
+++ src/main/groovy/org/mockito/release/internal/gradle/E2ETestingPlugin.java
@@ -6,9 +6,10 @@ import org.gradle.api.Project;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
 import org.gradle.api.tasks.*;
+import org.mockito.release.exec.*;
 
 import java.io.File;
-import java.util.Collection;
+import java.util.List;
 
 import static java.util.Arrays.asList;
 
@@ -36,24 +37,29 @@ public class E2ETestingPlugin implements Plugin<Project> {
 
         void create(String gitHubRepoUrl) {
             String repoName = extractRepoName(gitHubRepoUrl);
-            CloneGitHubRepositoryTask clone = project.getTasks().create("cloneProject" + repoName, CloneGitHubRepositoryTask.class);
+            CloneGitHubRepositoryTask clone = project.getTasks().create("cloneProjectFromGitHub" + repoName, CloneGitHubRepositoryTask.class);
             clone.setRepository(gitHubRepoUrl);
             clone.setTargetDir(new File(project.getBuildDir(), repoName + "-pristine"));
+            // For now for easier testing
+            clone.dependsOn("clean");
 
-            //TODO MS git clone from -pristine instead of copy?
+            // Clone from *-pristine to *-work. Copy task will not work because of ignoring git specific files:
+            // https://discuss.gradle.org/t/copy-git-specific-files/11970
             File workDir = new File(project.getBuildDir(), repoName + "-work");
-            Copy copy = project.getTasks().create("copyProject" + repoName, Copy.class);
+            CloneGitHubRepositoryTask copy = project.getTasks().create("cloneProjectToWorkDir" + repoName, CloneGitHubRepositoryTask.class);
             copy.dependsOn(clone);
-            copy.from(clone.getTargetDir());
-            copy.into(workDir);
+            copy.setRepository(clone.getTargetDir().getAbsolutePath());
+            copy.setTargetDir(workDir);
 
             RunTestTask run = project.getTasks().create("runTest" + repoName, RunTestTask.class);
             run.dependsOn(copy);
             run.setWorkDir(workDir);
+            run.setRepoName(repoName);
+
             //Using Gradle's composite builds ("--include-build") so that we're picking up current version of tools
             run.setCommand(asList("./gradlew", "publishToMavenLocal", "testRelease",
                     "-x", "gitPush", "-x", "bintrayUpload",
-                    "--include-build", project.getRootDir().getAbsolutePath()));
+                    "--include-build", project.getRootDir().getAbsolutePath(), "-s"));
 
             //we should put the build log in separate file instead of including it in the console of the parent build
             //otherwise the output will be really messy
@@ -66,23 +72,25 @@ public class E2ETestingPlugin implements Plugin<Project> {
         }
     }
 
-    public static class CloneGitHubRepositoryTask extends Exec {
+    public static class CloneGitHubRepositoryTask extends DefaultTask {
 
-        @Input private String repository;
-        @OutputDirectory private File targetDir;
+        private String repository;
+        private File targetDir;
 
         @TaskAction
         public void cloneRepository() {
-            LOG.lifecycle("  CloneGitHubRepositoryTask cloneRepository");
-            workingDir("$projectDir");
-            commandLine("git", "clone", repository, targetDir);
-            exec();
+            LOG.lifecycle("  Clone repository");
+            getProject().getBuildDir().mkdirs();    // build dir can be not created yet
+            ProcessRunner processRunner = org.mockito.release.exec.Exec.getProcessRunner(getProject().getBuildDir());
+            processRunner.run("git", "clone", repository, targetDir.getAbsolutePath());
         }
 
+        @Input
         public void setRepository(String repository) {
             this.repository = repository;
         }
 
+        @OutputDirectory
         public void setTargetDir(File targetDir) {
             this.targetDir = targetDir;
         }
@@ -95,19 +103,37 @@ public class E2ETestingPlugin implements Plugin<Project> {
 
     public static class RunTestTask extends DefaultTask {
 
-        private Collection<String> command;
+        private List<String> command;
+        private File buildOutput;
+        private File workDir;
+        private String repoName;
 
-        public void setWorkDir(File workDir) {
-
+        @TaskAction
+        public void runTest() {
+            LOG.lifecycle("  Run test of {}. The output will be save in {}", repoName, buildOutput.getAbsoluteFile());
+            ProcessRunner processRunner = org.mockito.release.exec.Exec.getProcessRunner(workDir, buildOutput);
+            processRunner.run(command);
         }
 
         @Input
-        public void setCommand(Collection<String> command) {
+        public void setWorkDir(File workDir) {
+            this.workDir = workDir;
+        }
+
+        @Input
+        public void setCommand(List<String> command) {
             this.command = command;
         }
 
+        @OutputFile
         public void setBuildOutputFile(File file) {
-
+            buildOutput = file;
         }
+
+        @Input
+        public void setRepoName(String repoName) {
+            this.repoName = repoName;
+        }
+
     }
 }
