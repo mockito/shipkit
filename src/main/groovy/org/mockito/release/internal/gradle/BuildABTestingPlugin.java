175a1c01c127f8e1aee42298509f8f8cce3fe5fa
epeee <epeee@users.noreply.github.com>
2017-05-27 06:15:25 +0200

    extract filedifferenceprovider added some tests fixed bug found by tests

--- src/main/groovy/org/mockito/release/internal/gradle/BuildABTestingPlugin.java
+++ src/main/groovy/org/mockito/release/internal/gradle/BuildABTestingPlugin.java
@@ -11,13 +11,13 @@ import org.gradle.api.tasks.*;
 import org.json.simple.Jsonable;
 import org.json.simple.Jsoner;
 import org.mockito.release.exec.DefaultProcessRunner;
+import org.mockito.release.internal.comparison.file.FileDifferenceProvider;
 import org.mockito.release.notes.util.IOUtil;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -120,68 +120,6 @@ public class BuildABTestingPlugin implements Plugin<Project> {
         }
     }
 
-    public static class FileDifferenceProvider {
-
-        public CompareResult getDifference(File dirA, File dirB) {
-            List<File> dirAFiles = getFilesResursive(dirA.listFiles());
-            Collections.sort(dirAFiles);
-            List<File> dirBFiles = getFilesResursive(dirB.listFiles());
-            Collections.sort(dirBFiles);
-
-            List<File> onlyA = new ArrayList<File>();
-            List<File> onlyB = new ArrayList<File>();
-            List<File> bothButDifferent = new ArrayList<File>();
-
-            int i = 0;
-            int j = 0;
-
-            while (dirAFiles.size() > i && dirBFiles.size() > j) {
-                String dirASubPath = dirAFiles.get(i).getPath().substring(dirA.getPath().length());
-                String dirBSubPath = dirBFiles.get(j).getPath().substring(dirB.getPath().length());
-                int compareResult = dirASubPath.compareTo(dirBSubPath);
-
-                if (compareResult < 0) {
-                    onlyA.add(dirAFiles.get(i));
-                    i++;
-                } else if (compareResult > 0) {
-                    onlyB.add(dirBFiles.get(j));
-                    j++;
-                } else {
-                    boolean sameMd5 = true;
-                    // TODO md5 check
-
-                    if (dirAFiles.get(i).length() == dirBFiles.get(j).length() && sameMd5) {
-                        // nothing to do, both files are available
-                    } else {
-                        bothButDifferent.add(dirAFiles.get(i));
-                        bothButDifferent.add(dirBFiles.get(j));
-                    }
-                    i++;
-                    j++;
-                }
-            }
-
-            CompareResult result = new CompareResult();
-            result.setOnlyA(onlyA);
-            result.setOnlyB(onlyB);
-            result.setBothButDifferent(bothButDifferent);
-
-            return result;
-        }
-
-
-        private List<File> getFilesResursive(File[] files) {
-            List<File> filesRecursive = new ArrayList<File>();
-            for (File file : files) {
-                filesRecursive.add(file);
-                if (file.isDirectory()) {
-                    filesRecursive.addAll(getFilesResursive(file.listFiles()));
-                }
-            }
-            return filesRecursive;
-        }
-    }
-
     public static class CompareResult implements Jsonable {
 
         private static final String JSON_FORMAT = "{ \"onlyA\": \"%s\", \"onlyB\": \"%s\", " +
@@ -212,7 +150,7 @@ public class BuildABTestingPlugin implements Plugin<Project> {
         }
 
         private List<String> toStringList(List<File> files) {
-            List<String> ret = new ArrayList<String>();
+            List<String> ret = new ArrayList<String>(files.size());
             for (File file : files) {
                 ret.add(file.getPath());
             }
---
+++ src/test/groovy/org/mockito/release/internal/comparison/file/FileDifferenceProviderTest.groovy
@@ -0,0 +1,74 @@
+package org.mockito.release.internal.comparison.file
+
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import org.mockito.release.internal.gradle.BuildABTestingPlugin
+import spock.lang.Specification
+
+
+class FileDifferenceProviderTest extends Specification {
+
+    @Rule
+    TemporaryFolder tmp = new TemporaryFolder()
+
+    File dirA, dirB
+
+    def setup() {
+        dirA = tmp.newFolder('dirA')
+        dirB = tmp.newFolder('dirB')
+    }
+
+    def "sameContent" () {
+        given:
+        createSomeSameContent()
+
+        when:
+        BuildABTestingPlugin.CompareResult result = new FileDifferenceProvider().getDifference(dirA, dirB);
+
+        then:
+        result.onlyA.isEmpty()
+        result.onlyB.isEmpty()
+        result.bothButDifferent.isEmpty()
+    }
+
+    def "onlyA" () {
+        given:
+        createSomeSameContent()
+        File dirC = new File(dirA, 'b/c')
+        dirC.mkdirs()
+        File fileD = new File(dirC, 'd')
+        fileD << 'content of d'
+
+        when:
+        BuildABTestingPlugin.CompareResult result = new FileDifferenceProvider().getDifference(dirA, dirB);
+
+        then:
+        result.onlyA == [dirC.parentFile, dirC, fileD]
+        result.onlyB.isEmpty()
+        result.bothButDifferent.isEmpty()
+    }
+
+    def "onlyB" () {
+        given:
+        createSomeSameContent()
+        File dirZ = new File(dirB, 'x/y/z')
+        dirZ.mkdirs()
+        File fileW = new File(dirZ, 'w')
+        fileW << 'content of d'
+
+        when:
+        BuildABTestingPlugin.CompareResult result = new FileDifferenceProvider().getDifference(dirA, dirB);
+
+        then:
+        result.onlyA.isEmpty()
+        result.onlyB == [dirZ.parentFile.parentFile, dirZ.parentFile, dirZ, fileW]
+        result.bothButDifferent.isEmpty()
+    }
+
+    private void createSomeSameContent() {
+        File dirAFile = new File(dirA, 'newFile')
+        dirAFile << "someContent"
+        File dirBFile = new File(dirB,'newFile')
+        dirBFile << 'someContent'
+    }
+}
