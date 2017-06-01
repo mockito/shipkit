03545ad2b165aaaf6ed9adf7bd4c3b68e2e3bf4a
epeee <epeee@users.noreply.github.com>
2017-06-01 19:22:24 +0200

    move java plugin handling to existing allprojects block

--- src/main/groovy/org/shipkit/internal/gradle/ContinuousDeliveryPlugin.java
+++ src/main/groovy/org/shipkit/internal/gradle/ContinuousDeliveryPlugin.java
@@ -52,19 +52,14 @@ public class ContinuousDeliveryPlugin implements Plugin<Project> {
 
         project.allprojects(new Action<Project>() {
             @Override
-            public void execute(final Project project) {
-                project.getPlugins().withId("java", new Action<Plugin>() {
+            public void execute(final Project subproject) {
+                subproject.getPlugins().withId("java", new Action<Plugin>() {
                     @Override
                     public void execute(Plugin plugin) {
                         project.getPlugins().apply(JavaLibraryPlugin.class);
                     }
                 });
-            }
-        });
 
-        project.allprojects(new Action<Project>() {
-            @Override
-            public void execute(final Project subproject) {
                 subproject.getPlugins().withType(BaseJavaLibraryPlugin.class, new Action<BaseJavaLibraryPlugin>() {
                     @Override
                     public void execute(BaseJavaLibraryPlugin p) {
