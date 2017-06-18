1d9765953a8ac7def8c0cfa92145810e1f86d285
Szczepan Faber <szczepiq@gmail.com>
2017-06-17 23:02:36 -0700

    Added init task to generate .travis.yml file

--- src/main/groovy/org/shipkit/internal/gradle/InitPlugin.java
+++ src/main/groovy/org/shipkit/internal/gradle/InitPlugin.java
@@ -4,7 +4,10 @@ import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
+import org.shipkit.gradle.InitTravisTask;
 import org.shipkit.internal.gradle.util.TaskMaker;
+
+import java.io.File;
 
 /**
  * Creates task initShipkit that all other init tasks should depend on
@@ -13,15 +16,23 @@ import org.shipkit.internal.gradle.util.TaskMaker;
 public class InitPlugin implements Plugin<Project> {
 
     public static final String INIT_SHIPKIT_TASK = "initShipkit";
+    public static final String INIT_TRAVIS_TASK = "initTravis";
 
     @Override
     public void apply(final Project project) {
-
-        TaskMaker.task(project, INIT_SHIPKIT_TASK, new Action<Task>() {
-            @Override
-            public void execute(Task t) {
-                t.setDescription("Initializes Shipkit");
+        TaskMaker.task(project, INIT_TRAVIS_TASK, InitTravisTask.class, new Action<InitTravisTask>() {
+            public void execute(InitTravisTask t) {
+                t.setDescription("Creates '.travis.yml' file if not already present.");
+                t.setOutputFile(new File(project.getRootDir(), ".travis.yml"));
             }
         });
+
+        TaskMaker.task(project, INIT_SHIPKIT_TASK, new Action<Task>() {
+            public void execute(Task t) {
+                t.setDescription("Initializes Shipkit");
+                t.dependsOn(INIT_TRAVIS_TASK);
+            }
+        });
+
     }
 }
