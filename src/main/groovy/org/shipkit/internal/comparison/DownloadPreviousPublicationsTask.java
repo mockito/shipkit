a0c6932a97423e2027e1e6c1dd507cfdc813377a
Szczepan Faber <szczepiq@gmail.com>
2017-07-03 02:22:15 -0700

    Moved the implementation out of the public type

--- src/main/groovy/org/shipkit/internal/comparison/DownloadPreviousPublicationsTask.java
+++ src/main/groovy/org/shipkit/internal/comparison/DownloadPreviousPublicationsTask.java
@@ -1,12 +1,10 @@
 package org.shipkit.internal.comparison;
 
 import org.gradle.api.DefaultTask;
-import org.gradle.api.logging.Logger;
-import org.gradle.api.logging.Logging;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
-import org.shipkit.internal.notes.util.IOUtil;
+import org.shipkit.internal.gradle.java.tasks.DownloadPreviousPublications;
 
 import java.io.File;
 
@@ -16,25 +14,15 @@ import java.io.File;
  */
 public class DownloadPreviousPublicationsTask extends DefaultTask {
 
-    private static final Logger LOG = Logging.getLogger(DownloadPreviousPublicationsTask.class);
-
     @Input private String previousPomUrl;
     @Input private String previousSourcesJarUrl;
 
     @OutputFile private File previousPom;
     @OutputFile private File previousSourcesJar;
 
-    @TaskAction public void downloadPreviousReleases(){
-        downloadRemoteFile(previousPomUrl, previousPom);
-        downloadRemoteFile(previousSourcesJarUrl, previousSourcesJar);
-    }
-
-    private void downloadRemoteFile(String remoteUrl, File localFile) {
-        LOG.lifecycle("Downloading remote artifact\n" +
-                "  - from {}\n" +
-                "  - and saving it to {}", remoteUrl, localFile);
-
-        IOUtil.downloadToFile(remoteUrl, localFile);
+    @TaskAction
+    public void downloadPreviousPublications() {
+        new DownloadPreviousPublications().downloadPreviousPublications(this);
     }
 
     /**
---
+++ src/main/groovy/org/shipkit/internal/gradle/java/tasks/DownloadPreviousPublications.java
@@ -0,0 +1,26 @@
+package org.shipkit.internal.gradle.java.tasks;
+
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
+import org.shipkit.internal.comparison.DownloadPreviousPublicationsTask;
+import org.shipkit.internal.notes.util.IOUtil;
+
+import java.io.File;
+
+public class DownloadPreviousPublications {
+
+    private static final Logger LOG = Logging.getLogger(DownloadPreviousPublications.class);
+
+    public void downloadPreviousPublications(DownloadPreviousPublicationsTask task) {
+        downloadRemoteFile(task.getPreviousPomUrl(), task.getPreviousPom());
+        downloadRemoteFile(task.getPreviousSourcesJarUrl(), task.getPreviousSourcesJar());
+    }
+
+    private void downloadRemoteFile(String remoteUrl, File localFile) {
+        LOG.lifecycle("Downloading remote artifact\n" +
+                "  - from {}\n" +
+                "  - and saving it to {}", remoteUrl, localFile);
+
+        IOUtil.downloadToFile(remoteUrl, localFile);
+    }
+}
