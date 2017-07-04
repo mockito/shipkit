448113ca48945cde34c7f87c5e7f27c2041637ce
Szczepan Faber <szczepiq@gmail.com>
2017-07-04 14:09:29 -0700

    Added missing javadoc

--- src/main/groovy/org/shipkit/gradle/java/ComparePublicationsTask.java
+++ src/main/groovy/org/shipkit/gradle/java/ComparePublicationsTask.java
@@ -1,9 +1,11 @@
 package org.shipkit.gradle.java;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.publish.maven.tasks.GenerateMavenPom;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.bundling.Jar;
@@ -14,13 +16,16 @@ import java.io.File;
 /**
  * Compares sources jars and pom files produced by the build with analogical artifacts
  * from last published build. If it determines that there were no changes it advises the user to
- * skip publication of the new version artifacts.
+ * skip publication of the new version artifacts (e.g. skip the release).
+ * <p>
+ * The outputs of this task are used by {@link org.shipkit.gradle.ReleaseNeededTask}.
+ * The {@link #getComparisonResult()} should be added to {@link org.shipkit.gradle.ReleaseNeededTask#addComparisonResult(File)}.
  */
 public class ComparePublicationsTask extends DefaultTask {
 
     @Input private String projectGroup;
     @Input private String currentVersion;
-    @Input private String previousVersion;
+    @Input @Optional private String previousVersion;
     @InputFiles private Jar sourcesJar;
     @Input private String pomTaskName;
 
@@ -30,7 +35,10 @@ public class ComparePublicationsTask extends DefaultTask {
     @OutputFile private File comparisonResult;
 
     /**
-     * File that stores text result of the comparison
+     * File that stores text result of the comparison.
+     * If the file is empty it means the publications are the same.
+     * If the file does not exist it means that the task did not run.
+     * In this case you can assume that there are differences and the release should be triggered.
      */
     public File getComparisonResult() {
         return comparisonResult;
@@ -47,6 +55,11 @@ public class ComparePublicationsTask extends DefaultTask {
         new ComparePublications().comparePublications(this);
     }
 
+    /**
+     * Sets the sourcesJar for comparision with {@link #getPreviousVersionSourcesJarFile()}.
+     * Task dependency will be automatically added from this task to sourcesJar task supplied as parameter.
+     * During comparison, the algorithm will read jar's output file using {@link Jar#getArchivePath()}.
+     */
     public void compareSourcesJar(Jar sourcesJar) {
         //when we compare, we can get the sources jar file via sourcesJar.archivePath
         this.sourcesJar = sourcesJar;
@@ -55,6 +68,12 @@ public class ComparePublicationsTask extends DefaultTask {
         this.dependsOn(sourcesJar);
     }
 
+    /**
+     * Sets the pom task name for comparision with {@link #getPreviousVersionSourcesJarFile()}.
+     * Task dependency will be automatically added from this task to pomTaskName supplied as parameter.
+     * During comparison, the algorithm will get the pom task, cast it to {@link GenerateMavenPom},
+     * and read {@link GenerateMavenPom#getDestination()}.
+     */
     public void comparePom(String pomTaskName) {
         this.pomTaskName = pomTaskName;
 
@@ -62,50 +81,92 @@ public class ComparePublicationsTask extends DefaultTask {
         this.dependsOn(pomTaskName);
     }
 
+    /**
+     * The artifact group, used during pom comparison to ignore version changes of artifacts with that group.
+     * Needed to make the pom comparison robust and avoid false positives.
+     */
     public String getProjectGroup() {
         return projectGroup;
     }
 
+    /**
+     * See {@link #getProjectGroup()}
+     */
     public void setProjectGroup(String projectGroup) {
         this.projectGroup = projectGroup;
     }
 
+    /**
+     * Current project version, used during pom comparison to ignore version changes of internal dependencies.
+     * Used to make pom comparison robust and avoid false positives.
+     */
     public String getCurrentVersion() {
         return currentVersion;
     }
 
+    /**
+     * See {@link #getCurrentVersion()}
+     */
     public void setCurrentVersion(String currentVersion) {
         this.currentVersion = currentVersion;
     }
 
+    /**
+     * Previous version, used during pom comparison to ignore version changes of internal dependencies.
+     * Used to make pom comparison robust and avoid false positives.
+     * <p>
+     * If null it means there was no previous version and the task will not run any diffs.
+     */
     public String getPreviousVersion() {
         return previousVersion;
     }
 
+    /**
+     * See {@link #getPreviousVersion()}
+     */
     public void setPreviousVersion(String previousVersion) {
         this.previousVersion = previousVersion;
     }
 
+    /**
+     * Previously released pom file used for comparison with currently built pom file.
+     */
     public File getPreviousVersionPomFile() {
         return previousVersionPomFile;
     }
 
+    /**
+     * See {@link #getPreviousVersionPomFile()}
+     */
     public void setPreviousVersionPomFile(File previousVersionPomFile) {
         this.previousVersionPomFile = previousVersionPomFile;
     }
 
+    /**
+     * Previously released sources jar used for comparison with currently built sources jar.
+     */
     public File getPreviousVersionSourcesJarFile() {
         return previousVersionSourcesJarFile;
     }
 
+    /**
+     * See {@link #getPreviousVersionSourcesJarFile()}
+     */
     public void setPreviousVersionSourcesJarFile(File previousVersionSourcesJarFile) {
         this.previousVersionSourcesJarFile = previousVersionSourcesJarFile;
     }
 
+    /**
+     * Currently built sources jar file used for comparison.
+     */
     public Jar getSourcesJar() {
         return sourcesJar;
     }
 
+    /**
+     * Pom task name that builds current pom file.
+     * The task must be of type {@link GenerateMavenPom}.
+     */
     public String getPomTaskName() {
         return pomTaskName;
     }
