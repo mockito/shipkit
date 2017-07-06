92958fd2ab4490252fb6a6de2334c0a1746cd8ee
Szczepan Faber <szczepiq@gmail.com>
2017-07-05 21:43:56 -0700

    Updated TODO

--- src/main/groovy/org/shipkit/gradle/ReleaseNeededTask.java
+++ src/main/groovy/org/shipkit/gradle/ReleaseNeededTask.java
@@ -19,7 +19,7 @@ import java.util.List;
  *  - the current Git branch does not match release-eligibility regex ({@link #getReleasableBranchRegex()}.
  *  - binaries have not changes since the previous release
  */
-//TODO move to 'release' subpackage
+//TODO move to 'release' subpackage and also move surrounding classes to better packages
 public class ReleaseNeededTask extends DefaultTask {
 
     private String branch;
