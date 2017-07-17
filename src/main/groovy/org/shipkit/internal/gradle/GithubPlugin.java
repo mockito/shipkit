54c25f46dfaac2f907fddb552f78c0162da81823
epeee <epeee@users.noreply.github.com>
2017-07-17 20:52:26 +0200

    javadoc

--- src/main/groovy/org/shipkit/internal/gradle/GithubPlugin.java
+++ src/main/groovy/org/shipkit/internal/gradle/GithubPlugin.java
@@ -5,7 +5,17 @@ import org.gradle.api.Project;
 import org.shipkit.internal.gradle.contributors.github.GithubContributorsPlugin;
 import org.shipkit.internal.gradle.java.GithubPomContributorsPlugin;
 
-
+/**
+ * This plugin applies other plugins which make shipkit work with Github.
+ * <p>
+ * Applies following plugins:
+ *
+ * <ul>
+ *     <li>{@link GithubContributorsPlugin}</li>
+ *     <li>{@link GithubPomContributorsPlugin}</li>
+ * </ul>
+ *
+ */
 public class GithubPlugin implements Plugin<Project> {
 
     @Override
