ea8523593fca1034d2047bbfa9fd9c33c712c2fc
epeee <epeee@users.noreply.github.com>
2017-08-03 16:03:50 +0200

    added todo

--- src/main/groovy/org/shipkit/internal/notes/contributors/ConcurrentDispatcher.java
+++ src/main/groovy/org/shipkit/internal/notes/contributors/ConcurrentDispatcher.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+//TODO move to util and add some javadoc
 public class ConcurrentDispatcher {
 
     private static final int N_THREADS = 4;
