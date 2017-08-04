e6a4fe691332c5339a9f66760e751d2e9dce3afa
Adrian Nicos <adrian.nicos@gmail.com>
2017-08-04 21:19:46 +0200

    Removing unused imports

--- src/main/groovy/org/shipkit/internal/gradle/util/FileUtil.groovy
+++ src/main/groovy/org/shipkit/internal/gradle/util/FileUtil.groovy
@@ -1,7 +1,6 @@
 package org.shipkit.internal.gradle.util
 
 import groovy.transform.CompileStatic
-import org.shipkit.internal.notes.header.HeaderProvider
 import org.shipkit.internal.notes.header.HeaderRemover
 
 /**
@@ -13,25 +12,14 @@ class FileUtil {
     //TODO (maybe) convert to Java at some point
 
     /**
-     * Returns first line of the file.
-     */
-    static String firstLine(File notesFile) {
-        return notesFile.withReader { it.readLine() }
-    }
-
-    /**
      * Appends content to the top of the file.
      */
     static void appendToTop(String content, File notesFile) {
         notesFile.getParentFile().mkdirs()
         notesFile.createNewFile()
         def existing = notesFile.text
-        def existingWithoutHeader =  HeaderRemover.removeHeaderIfExist(existing)
+        def existingWithoutHeader = HeaderRemover.removeHeaderIfExist(existing)
 
         notesFile.text = content + existingWithoutHeader
-    }
-
-    private static String removeHeader(String existing) {
-        return existing.replaceFirst(HeaderProvider.HEADER_START + ".*" + HeaderProvider.HEADER_END+"[\\r\\n]+", "")
     }
 }
--- src/test/groovy/org/shipkit/internal/gradle/notes/tasks/UpdateReleaseNotesTaskTest.groovy
+++ src/test/groovy/org/shipkit/internal/gradle/notes/tasks/UpdateReleaseNotesTaskTest.groovy
@@ -3,16 +3,16 @@ package org.shipkit.internal.gradle.notes.tasks
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 import org.shipkit.internal.notes.header.HeaderProvider
-import org.shipkit.internal.notes.header.HeaderRemover
 import spock.lang.Specification
 
 class UpdateReleaseNotesTaskTest extends Specification {
 
-    @Rule TemporaryFolder tmp = new TemporaryFolder()
+    @Rule
+    TemporaryFolder tmp = new TemporaryFolder()
 
     UpdateReleaseNotes update = new UpdateReleaseNotes(new HeaderProvider())
 
-    def "should update release notes if not in preview mode" (){
+    def "should update release notes if not in preview mode"() {
         def f = tmp.newFile("release-notes.md")
 
         when:
@@ -22,7 +22,7 @@ class UpdateReleaseNotesTaskTest extends Specification {
         f.text == "content"
     }
 
-    def "should not modify releaseNotesFile if in preview mode" (){
+    def "should not modify releaseNotesFile if in preview mode"() {
         def f = tmp.newFile("release-notes.md")
 
         when:
