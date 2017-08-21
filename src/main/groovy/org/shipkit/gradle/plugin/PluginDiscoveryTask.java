8f217bb73244bd2377621843065a4ad1459e7b5f
epeee <epeee@users.noreply.github.com>
2017-08-21 19:04:56 +0200

    javadoc

--- src/main/groovy/org/shipkit/gradle/plugin/PluginDiscoveryTask.java
+++ src/main/groovy/org/shipkit/gradle/plugin/PluginDiscoveryTask.java
@@ -1,9 +1,13 @@
 package org.shipkit.gradle.plugin;
 
+import com.gradle.publish.PluginBundleExtension;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
 import org.shipkit.internal.gradle.plugin.tasks.PluginDiscovery;
 
+/**
+ * Discovers gradle plugins and adds them to the {@link PluginBundleExtension}.
+ */
 public class PluginDiscoveryTask extends DefaultTask {
 
     @TaskAction
