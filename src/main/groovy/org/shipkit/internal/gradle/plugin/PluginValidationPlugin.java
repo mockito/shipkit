e8ad451a49c6d7ae5e8d0225882c53b2f0cd839e
epeee <epeee@users.noreply.github.com>
2017-08-28 07:36:04 +0200

    make use of latest shipkit version (0.9.52) and move check dependsOn vP to shipkit.gradle

--- src/main/groovy/org/shipkit/internal/gradle/plugin/PluginValidationPlugin.java
+++ src/main/groovy/org/shipkit/internal/gradle/plugin/PluginValidationPlugin.java
@@ -3,8 +3,6 @@ package org.shipkit.internal.gradle.plugin;
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.Task;
-import org.gradle.api.plugins.JavaBasePlugin;
 import org.shipkit.gradle.plugin.PluginValidatorTask;
 import org.shipkit.internal.gradle.util.JavaPluginUtil;
 import org.shipkit.internal.gradle.util.TaskMaker;
@@ -27,16 +25,13 @@ public class PluginValidationPlugin implements Plugin<Project> {
         project.getPlugins().withId("java", new Action<Plugin>() {
             @Override
             public void execute(Plugin plugin) {
-                final Task task = TaskMaker.task(project, VALIDATE_PLUGINS, PluginValidatorTask.class, new Action<PluginValidatorTask>() {
+                TaskMaker.task(project, VALIDATE_PLUGINS, PluginValidatorTask.class, new Action<PluginValidatorTask>() {
                     @Override
                     public void execute(PluginValidatorTask task) {
                         task.setDescription("Validates Gradle Plugins and their properties files");
                         task.setSourceSet(JavaPluginUtil.getMainSourceSet(project));
                     }
                 });
-
-                final Task check = project.getTasks().getByName(JavaBasePlugin.CHECK_TASK_NAME);
-                check.dependsOn(task);
             }
         });
     }
