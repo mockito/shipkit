c8f4e3cfb38227286df73ac95491c18dfc8286a7
epeee <epeee@users.noreply.github.com>
2017-08-30 06:41:26 +0200

    fix logging for authToken

--- src/test/groovy/org/shipkit/internal/gradle/git/tasks/TokenAvailabilityMessageTest.groovy
+++ src/test/groovy/org/shipkit/internal/gradle/git/tasks/TokenAvailabilityMessageTest.groovy
@@ -6,7 +6,7 @@ class TokenAvailabilityMessageTest extends Specification {
 
     def "creates message"() {
         expect:
-        TokenAvailabilityMessage.createMessage("git push", null) == "  'git push' uses GitHub write token"
-        TokenAvailabilityMessage.createMessage("git push", "secret") == "  'git push' does not use GitHub write token because it was not specified"
+        TokenAvailabilityMessage.createMessage("git push", null) == "  'git push' does not use GitHub write token because it was not specified"
+        TokenAvailabilityMessage.createMessage("git push", "secret") == "  'git push' uses GitHub write token"
     }
 }
