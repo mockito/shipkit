5855d4658bd998e5c070c82e03d3aa90adeaa9b2
epeee <epeee@users.noreply.github.com>
2017-09-07 21:43:56 +0200

    add some tests

--- src/test/groovy/org/shipkit/internal/gradle/notes/tasks/UpdateReleaseNotesTaskTest.groovy
+++ src/test/groovy/org/shipkit/internal/gradle/notes/tasks/UpdateReleaseNotesTaskTest.groovy
@@ -1,7 +1,9 @@
 package org.shipkit.internal.gradle.notes.tasks
 
+import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
+import org.shipkit.gradle.notes.UpdateReleaseNotesTask
 import spock.lang.Specification
 
 class UpdateReleaseNotesTaskTest extends Specification {
@@ -29,5 +31,25 @@ class UpdateReleaseNotesTaskTest extends Specification {
 
         then:
         f.text.isEmpty()
+    }
+
+    def "check release notes url"(branch, expectedUrl) {
+        given:
+        def task = Mock(UpdateReleaseNotesTask)
+        def project = new ProjectBuilder().withName("myProject").withProjectDir(tmp.root).build()
+
+        when:
+        task.gitHubUrl >> "https://github.com"
+        task.gitHubRepository >> 'mockito/mockito'
+        task.project >> project
+        task.releaseNotesFile >> project.file("doc/release-notes/official.md")
+
+        then:
+        expectedUrl == update.getReleaseNotesUrl(task, branch)
+
+        where:
+        branch          | expectedUrl
+        'master'        | 'https://github.com/mockito/mockito/blob/master/doc/release-notes/official.md'
+        'release/2.x'   | 'https://github.com/mockito/mockito/blob/release/2.x/doc/release-notes/official.md'
     }
 }
