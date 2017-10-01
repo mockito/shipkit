5b7deac682d81fa9bfbe56c8eb8c4d6b342d9d96
Szczepan Faber <szczepiq@gmail.com>
2017-10-01 07:15:37 -0700

    Added missing unit test
    The goal is to help @StefMa with #446

--- subprojects/shipkit/src/test/groovy/org/shipkit/internal/gradle/bintray/BintrayReleasePluginTest.groovy
+++ subprojects/shipkit/src/test/groovy/org/shipkit/internal/gradle/bintray/BintrayReleasePluginTest.groovy
@@ -1,17 +1,25 @@
 package org.shipkit.internal.gradle.bintray
 
-import org.gradle.testfixtures.ProjectBuilder
+import org.shipkit.gradle.notes.UpdateReleaseNotesTask
 import org.shipkit.internal.gradle.java.JavaBintrayPlugin
-import spock.lang.Specification
+import org.shipkit.internal.gradle.notes.ReleaseNotesPlugin
+import testutil.PluginSpecification
 
-class BintrayReleasePluginTest extends Specification {
+class BintrayReleasePluginTest extends PluginSpecification {
 
-    def project = new ProjectBuilder().build()
-
-    def "applies"() {
-        expect:
+    def "configures tasks"() {
         project.plugins.apply(BintrayReleasePlugin)
         project.plugins.apply(JavaBintrayPlugin)
+
+        project.bintray.pkg.userOrg = "some-org"
+        project.bintray.pkg.repo = "some-repo"
+        project.bintray.pkg.name = "some-pkg"
+
+        when:
         project.evaluate()
+
+        then:
+        UpdateReleaseNotesTask updateNotes = project.tasks.getByName(ReleaseNotesPlugin.UPDATE_NOTES_TASK)
+        updateNotes.publicationRepository == "https://bintray.com/some-org/some-repo/some-pkg"
     }
 }
