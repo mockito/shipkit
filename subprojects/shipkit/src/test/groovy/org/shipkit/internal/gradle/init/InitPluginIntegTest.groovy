4e4e0f1d3cc152a9bd41628af6a01ac433e8f86c
wilk.wojtek@gmail.com <wilk.wojtek@gmail.com>
2017-10-28 09:24:45 +0200

    Redundant assertion and config removed from InitPluginIntegTest

--- subprojects/shipkit/src/test/groovy/org/shipkit/internal/gradle/init/InitPluginIntegTest.groovy
+++ subprojects/shipkit/src/test/groovy/org/shipkit/internal/gradle/init/InitPluginIntegTest.groovy
@@ -1,26 +1,17 @@
 package org.shipkit.internal.gradle.init
 
-import org.gradle.testkit.runner.BuildResult
 import testutil.GradleSpecification
 
 class InitPluginIntegTest extends GradleSpecification {
 
         def "runs initShipkit task in a project without any Shipkit configuration"() {
             given:
-            projectDir.newFolder("gradle")
-
             buildFile << """
             apply plugin: "org.shipkit.java"
         """
 
             expect:
-            BuildResult result = pass("initShipkit", "-s")
-
-            result.tasks.collect { it.path }.join("\n") == """:identifyGitOrigin
-:initShipkitFile
-:initTravis
-:initVersioning
-:initShipkit"""
+            pass("initShipkit", "-s")
 
             // check if configuration files were generated
             new File(projectDir.root, "gradle/shipkit.gradle").exists()
