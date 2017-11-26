8e29a6f8b8da412fed35974552be344285e5a893
Artur Skowronski <a.skowronski@smartrecruiters.com>
2017-11-26 09:04:55 +0100

    [SHIPKIT-375] Matching more cases after tests with shipkit-example

--- subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/git/tasks/GitPush.java
+++ subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/git/tasks/GitPush.java
@@ -1,5 +1,8 @@
 package org.shipkit.internal.gradle.git.tasks;
 
+import org.gradle.api.GradleException;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
 import org.shipkit.gradle.git.GitPushTask;
 import org.shipkit.internal.exec.DefaultProcessRunner;
 import org.shipkit.internal.gradle.util.CannotPushToGithubException;
@@ -11,6 +14,7 @@ import java.util.List;
  * Utility class for configuring git push task with the correct git push arguments.
  */
 public class GitPush {
+    private final static Logger LOG = Logging.getLogger(TokenAvailabilityMessage.class);
 
     /**
      * Constructs git push arguments based of the url, targets and dry run
@@ -33,12 +37,13 @@ public class GitPush {
             new DefaultProcessRunner(task.getProject().getProjectDir())
                 .setSecretValue(task.getSecretValue())
                 .run(GitPush.gitPushArgs(task.getUrl(), task.getTargets(), task.isDryRun()));
-        } catch (RuntimeException e) {
-            if (e.getMessage().contains("Authentication failed")) {
+        } catch (GradleException e) {
+            if (CannotPushToGithubException.matchException(e)) {
                 throw CannotPushToGithubException.create(e, task);
             } else {
                 throw e;
             }
         }
     }
+
 }
