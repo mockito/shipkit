9db1a828e3ea82ef1e6035636b0ed04aba543cf8
Artur Skowronski <a.skowronski@smartrecruiters.com>
2017-11-26 07:51:41 +0100

    Handling Exception and differentiate cases

---
+++ subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/util/CannotPushToGithubException.java
@@ -0,0 +1,22 @@
+package org.shipkit.internal.gradle.util;
+
+import org.shipkit.gradle.git.GitPushTask;
+
+public class CannotPushToGithubException extends RuntimeException {
+    public static final String GH_WRITE_TOKEN_NOT_SET_MSG = "GH_WRITE_TOKEN not set and you don't have access to push to remote. which is a possible issue. Please recheck your configuration.";
+    public static final String GH_WRITE_TOKEN_INVALID_MSG = "GH_WRITE_TOKEN is set but possibly invalid. Please recheck your configuration.";
+
+    private CannotPushToGithubException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public static CannotPushToGithubException create(Exception e, GitPushTask task) {
+        String message;
+        if (task.getSecretValue() == null) {
+            message = GH_WRITE_TOKEN_NOT_SET_MSG;
+        } else {
+            message = GH_WRITE_TOKEN_INVALID_MSG;
+        }
+        return new CannotPushToGithubException(message, e);
+    }
+}
