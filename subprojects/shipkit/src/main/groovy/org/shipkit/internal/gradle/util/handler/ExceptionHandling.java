3da9b379151877df6daff0271f8b7b1fc7f55391
Szczepan Faber <szczepiq@gmail.com>
2017-12-02 13:23:23 -0800

    Added javadoc

--- subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/util/handler/ExceptionHandling.java
+++ subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/util/handler/ExceptionHandling.java
@@ -2,6 +2,9 @@ package org.shipkit.internal.gradle.util.handler;
 
 import org.gradle.api.Action;
 
+/**
+ * Helps handling exceptions in a way the code is easy to test
+ */
 public class ExceptionHandling {
 
     public static void withExceptionHandling(Runnable code, Action<RuntimeException> handler) {
