5db8409dffadeac80295ced19f1e5bf4c30e2a24
Jonas Hoglin <jonashoglin1978@gmail.com>
2017-12-14 21:59:30 +0100

    Use the setting for github url when building the url to use

--- subprojects/shipkit/src/test/groovy/org/shipkit/internal/gradle/git/GitHubUrlBuilderTest.groovy
+++ subprojects/shipkit/src/test/groovy/org/shipkit/internal/gradle/git/GitHubUrlBuilderTest.groovy
@@ -14,8 +14,28 @@ class GitHubUrlBuilderTest extends Specification {
         GitHubUrlBuilder.getGitHubUrl("org/repo", conf) == "https://user:token@github.com/org/repo.git"
     }
 
+    def "should return Custom GH url whith auth"() {
+        def conf = new ShipkitConfiguration()
+        conf.gitHub.writeAuthToken = "token"
+        conf.gitHub.writeAuthUser = "user"
+        conf.gitHub.url = "https://github.enterprise.com:123456"
+
+        expect:
+        GitHubUrlBuilder.getGitHubUrl("org/repo", conf) == "https://user:token@github.enterprise.com:123456/org/repo.git"
+    }
+
+    def "should return Custom GH url whith auth, even with trailing /"() {
+        def conf = new ShipkitConfiguration()
+        conf.gitHub.writeAuthToken = "token"
+        conf.gitHub.writeAuthUser = "user"
+        conf.gitHub.url = "http://192.168.0.199/"
+
+        expect:
+        GitHubUrlBuilder.getGitHubUrl("org/repo", conf) == "http://user:token@192.168.0.199/org/repo.git"
+    }
+
     def "should return GH url without auth by default"() {
         expect:
-        GitHubUrlBuilder._getGitHubUrl(null, "org/repo", null) == "https://github.com/org/repo.git"
+        GitHubUrlBuilder._getGitHubUrl(null, null, "org/repo", null) == "https://github.com/org/repo.git"
     }
 }
