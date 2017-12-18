4657f8fc42a1f137dec93a4a6a87232a446bf3ad
Jonas Hoglin <jonashoglin1978@gmail.com>
2017-12-18 09:59:41 +0100

    Alway suse config url and use spock datadriven tests to reduce code

--- subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/git/GitHubUrlBuilder.java
+++ subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/git/GitHubUrlBuilder.java
@@ -23,11 +23,11 @@ class GitHubUrlBuilder {
      */
     @ExposedForTesting
     static String _getGitHubUrl(String ghUrl, String ghUser, String ghRepo, String writeToken) {
+        String[] parts =  ghUrl.split("://");
         if (writeToken != null) {
-            String[] parts =  ghUrl.split("://");
             return MessageFormat.format("{0}://{1}:{2}@{3}/{4}.git", parts[0], ghUser, writeToken, parts[1], ghRepo);
         } else {
-            return MessageFormat.format("https://github.com/{0}.git", ghRepo);
+            return MessageFormat.format("{0}://{1}/{2}.git", parts[0], parts[1], ghRepo);
         }
     }
 }
--- subprojects/shipkit/src/test/groovy/org/shipkit/internal/gradle/git/GitHubUrlBuilderTest.groovy
+++ subprojects/shipkit/src/test/groovy/org/shipkit/internal/gradle/git/GitHubUrlBuilderTest.groovy
@@ -2,40 +2,34 @@ package org.shipkit.internal.gradle.git
 
 import org.shipkit.gradle.configuration.ShipkitConfiguration
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class GitHubUrlBuilderTest extends Specification {
 
-    def "should return GH url with auth"() {
-        def conf = new ShipkitConfiguration()
-        conf.gitHub.writeAuthToken = "token"
-        conf.gitHub.writeAuthUser = "user"
+    public static final String DEFAULT = "USE_DEFAULT"
+    def conf = new ShipkitConfiguration()
 
-        expect:
-        GitHubUrlBuilder.getGitHubUrl("org/repo", conf) == "https://user:token@github.com/org/repo.git"
-    }
+    @Unroll
+    def "should return GH url given gHurl '#ghUrl' repo '#repo' user '#user' token '#token'"() {
+        when :
+        if (ghUrl.equalsIgnoreCase(DEFAULT) == false) {
+            conf.gitHub.url = ghUrl;
+        }
+        if (user.equalsIgnoreCase(DEFAULT) == false) {
+            conf.gitHub.writeAuthUser = user
+        }
+        if (token.equalsIgnoreCase(DEFAULT) == false) {
+            conf.gitHub.writeAuthToken = token
+        }
 
-    def "should return Custom GH url whith auth"() {
-        def conf = new ShipkitConfiguration()
-        conf.gitHub.writeAuthToken = "token"
-        conf.gitHub.writeAuthUser = "user"
-        conf.gitHub.url = "https://github.enterprise.com:123456"
+        then:
+        GitHubUrlBuilder.getGitHubUrl("org/repo", conf) == expected
 
-        expect:
-        GitHubUrlBuilder.getGitHubUrl("org/repo", conf) == "https://user:token@github.enterprise.com:123456/org/repo.git"
-    }
-
-    def "should return Custom GH url whith auth, even with trailing /"() {
-        def conf = new ShipkitConfiguration()
-        conf.gitHub.writeAuthToken = "token"
-        conf.gitHub.writeAuthUser = "user"
-        conf.gitHub.url = "http://192.168.0.199/"
-
-        expect:
-        GitHubUrlBuilder.getGitHubUrl("org/repo", conf) == "http://user:token@192.168.0.199/org/repo.git"
-    }
-
-    def "should return GH url without auth by default"() {
-        expect:
-        GitHubUrlBuilder._getGitHubUrl(null, null, "org/repo", null) == "https://github.com/org/repo.git"
+        where:
+        repo        | token   | user    | ghUrl                     | expected
+        "org/repo"  | DEFAULT | DEFAULT | DEFAULT                   | "https://github.com/org/repo.git"
+        "org/repo"  | "token" | "user"  | DEFAULT                   | "https://user:token@github.com/org/repo.git"
+        "org/repo"  | DEFAULT | DEFAULT | "https://gh.ent.com:8080" | "https://gh.ent.com:8080/org/repo.git"
+        "org/repo"  | "token" | "user"  | "https://gh.ent.com:8080" | "https://user:token@gh.ent.com:8080/org/repo.git"
     }
 }
