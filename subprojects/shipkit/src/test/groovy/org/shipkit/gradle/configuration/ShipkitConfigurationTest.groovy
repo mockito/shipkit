6f87c6a2ae8b70a9561195b4301b68e0214c993f
epeee <epeee@users.noreply.github.com>
2017-12-22 07:05:57 +0100

    move ShipkitConfigurationTests to proper package

--- subprojects/shipkit/src/test/groovy/org/shipkit/gradle/configuration/ShipkitConfigurationTest.groovy
+++ subprojects/shipkit/src/test/groovy/org/shipkit/gradle/configuration/ShipkitConfigurationTest.groovy
@@ -1,7 +1,6 @@
-package org.shipkit.internal.gradle.configuration
+package org.shipkit.gradle.configuration
 
 import org.gradle.api.GradleException
-import org.shipkit.gradle.configuration.ShipkitConfiguration
 import org.shipkit.internal.gradle.util.team.TeamParser
 import spock.lang.Specification
 
