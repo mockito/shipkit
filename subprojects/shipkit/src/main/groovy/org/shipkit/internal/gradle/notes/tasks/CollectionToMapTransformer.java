01c2a1ba720aa05a882da478362be92c1b4bab70
epeee <epeee@users.noreply.github.com>
2018-01-06 09:03:17 +0100

    fix checkstyle violations

--- subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/notes/tasks/CollectionToMapTransformer.java
+++ subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/notes/tasks/CollectionToMapTransformer.java
@@ -6,18 +6,18 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CollectionToMapTransformer<A extends Collection<B>, B, K,V> {
+public class CollectionToMapTransformer<A extends Collection<B>, B, K, V> {
 
-    private final Function<B, Map.Entry<K,V>> transformFunction;
+    private final Function<B, Map.Entry<K, V>> transformFunction;
 
-    public CollectionToMapTransformer(Function<B, Map.Entry<K,V>> transformFunction) {
+    public CollectionToMapTransformer(Function<B, Map.Entry<K, V>> transformFunction) {
         this.transformFunction = transformFunction;
     }
 
-    public Map<K,V> transform(A collection) {
+    public Map<K, V> transform(A collection) {
         Map<K, V> contributorMap = new HashMap<K, V>();
         for (B b : collection) {
-            Map.Entry<K,V> entry = transformFunction.apply(b);
+            Map.Entry<K, V> entry = transformFunction.apply(b);
             contributorMap.put(entry.getKey(), entry.getValue());
         }
         return contributorMap;
