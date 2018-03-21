5e45ea3c8d2d4a2184cf5ebc393a090421991f51
Szczepan Faber <szczepiq@gmail.com>
2018-03-20 22:37:56 -0700

    Java8 Lambda for clarity

--- subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/bintray/BintrayReleasePlugin.java
+++ subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/bintray/BintrayReleasePlugin.java
@@ -1,7 +1,6 @@
 package org.shipkit.internal.gradle.bintray;
 
 import com.jfrog.bintray.gradle.BintrayExtension;
-import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
@@ -39,42 +38,33 @@ public class BintrayReleasePlugin implements Plugin<Project> {
         project.getPlugins().apply(ReleasePlugin.class);
         final ShipkitConfiguration conf = project.getPlugins().apply(ShipkitConfigurationPlugin.class).getConfiguration();
 
-        project.allprojects(new Action<Project>() {
-            @Override
-            public void execute(final Project subproject) {
-                subproject.getPlugins().withType(JavaBintrayPlugin.class, new Action<JavaBintrayPlugin>() {
-                    public void execute(JavaBintrayPlugin plugin) {
-                        Task bintrayUpload = subproject.getTasks().getByName(ShipkitBintrayPlugin.BINTRAY_UPLOAD_TASK);
-                        Task performRelease = project.getTasks().getByName(ReleasePlugin.PERFORM_RELEASE_TASK);
-                        performRelease.dependsOn(bintrayUpload);
+        project.allprojects(subproject -> subproject.getPlugins().withType(JavaBintrayPlugin.class, plugin -> {
+            Task bintrayUpload = subproject.getTasks().getByName(ShipkitBintrayPlugin.BINTRAY_UPLOAD_TASK);
+            Task performRelease = project.getTasks().getByName(ReleasePlugin.PERFORM_RELEASE_TASK);
+            performRelease.dependsOn(bintrayUpload);
 
-                        //Making git push run as late as possible because it is an operation that is hard to reverse.
-                        //Git push will be executed after all tasks needed by bintrayUpload
-                        // but before bintrayUpload.
-                        //Using task path as String because the task comes from maven-publish new configuration model
-                        // and we cannot refer to it in a normal way, by task instance.
-                        String mavenLocalTask = subproject.getPath() + ":" + MAVEN_LOCAL_TASK;
-                        Task gitPush = project.getTasks().getByName(GitPlugin.GIT_PUSH_TASK);
-                        gitPush.mustRunAfter(mavenLocalTask);
-                        //bintray upload after git push so that when git push fails we don't publish jars to bintray
-                        //git push is easier to undo than deleting published jars (not possible with Central)
-                        bintrayUpload.mustRunAfter(gitPush);
+            //Making git push run as late as possible because it is an operation that is hard to reverse.
+            //Git push will be executed after all tasks needed by bintrayUpload
+            // but before bintrayUpload.
+            //Using task path as String because the task comes from maven-publish new configuration model
+            // and we cannot refer to it in a normal way, by task instance.
+            String mavenLocalTask = subproject.getPath() + ":" + MAVEN_LOCAL_TASK;
+            Task gitPush = project.getTasks().getByName(GitPlugin.GIT_PUSH_TASK);
+            gitPush.mustRunAfter(mavenLocalTask);
+            //bintray upload after git push so that when git push fails we don't publish jars to bintray
+            //git push is easier to undo than deleting published jars (not possible with Central)
+            bintrayUpload.mustRunAfter(gitPush);
 
-                        final BintrayExtension bintray = subproject.getExtensions().getByType(BintrayExtension.class);
-                        deferredConfiguration(subproject, new Runnable() {
-                            public void run() {
-                                UpdateReleaseNotesTask updateNotes = (UpdateReleaseNotesTask) project.getTasks().getByName(ReleaseNotesPlugin.UPDATE_NOTES_TASK);
-                                String userSpecifiedRepo = conf.getLenient().getReleaseNotes().getPublicationRepository();
-                                if (userSpecifiedRepo != null) {
-                                    updateNotes.setPublicationRepository(userSpecifiedRepo);
-                                } else {
-                                    updateNotes.setPublicationRepository(BintrayUtil.getRepoLink(bintray));
-                                }
-                            }
-                        });
-                    }
-                });
-            }
-        });
+            final BintrayExtension bintray = subproject.getExtensions().getByType(BintrayExtension.class);
+            deferredConfiguration(subproject, () -> {
+                UpdateReleaseNotesTask updateNotes = (UpdateReleaseNotesTask) project.getTasks().getByName(ReleaseNotesPlugin.UPDATE_NOTES_TASK);
+                String userSpecifiedRepo = conf.getLenient().getReleaseNotes().getPublicationRepository();
+                if (userSpecifiedRepo != null) {
+                    updateNotes.setPublicationRepository(userSpecifiedRepo);
+                } else {
+                    updateNotes.setPublicationRepository(BintrayUtil.getRepoLink(bintray));
+                }
+            });
+        }));
     }
 }
