83fd005ee3084742fa9392eb05d965676526e14b
arturskowronski <me@arturskowronski.com>
2018-03-22 22:54:21 +0100

    [SHIPKIT-513] Initial solution using Travis ENV

--- subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/release/TravisPlugin.java
+++ subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/release/TravisPlugin.java
@@ -5,10 +5,12 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
+import org.shipkit.gradle.configuration.ShipkitConfiguration;
 import org.shipkit.gradle.git.IdentifyGitBranchTask;
 import org.shipkit.gradle.release.ReleaseNeededTask;
 import org.shipkit.internal.gradle.configuration.BasicValidator;
 import org.shipkit.internal.gradle.configuration.LazyConfiguration;
+import org.shipkit.internal.gradle.configuration.ShipkitConfigurationPlugin;
 import org.shipkit.internal.gradle.git.GitBranchPlugin;
 import org.shipkit.internal.gradle.git.GitSetupPlugin;
 import org.shipkit.internal.gradle.git.tasks.GitCheckOutTask;
@@ -42,7 +44,7 @@ public class TravisPlugin implements Plugin<Project> {
 
         final String branch = System.getenv("TRAVIS_BRANCH");
         LOG.info("Branch from 'TRAVIS_BRANCH' env variable: {}", branch);
-
+        ShipkitConfiguration conf = project.getPlugins().apply(ShipkitConfigurationPlugin.class).getConfiguration();
         //configure branch based on Travis' env variable
         IdentifyGitBranchTask identifyBranch = (IdentifyGitBranchTask) project.getTasks().getByName(GitBranchPlugin.IDENTIFY_GIT_BRANCH);
         if (!StringUtil.isEmpty(branch)) {
@@ -52,15 +54,11 @@ public class TravisPlugin implements Plugin<Project> {
         //set the branch to be checked out on ci build
         final GitCheckOutTask checkout = (GitCheckOutTask) project.getTasks().getByName(GitSetupPlugin.CHECKOUT_TASK);
         checkout.setRev(branch);
-        LazyConfiguration.lazyConfiguration(checkout, new Runnable() {
-            public void run() {
-                BasicValidator.notNull(checkout.getRev(),
-                        "Task " + checkout.getPath() + " does not know the target revision to check out.\n" +
-                        "In Travis CI builds, it is automatically configured from 'TRAVIS_BRANCH' environment variable.\n" +
-                        "If you are trying to run this task outside Travis, you can export the environment variable.\n" +
-                        "Alternatively, you can set the task's 'rev' property explicitly.");
-            }
-        });
+        LazyConfiguration.lazyConfiguration(checkout, () -> BasicValidator.notNull(checkout.getRev(),
+                "Task " + checkout.getPath() + " does not know the target revision to check out.\n" +
+                "In Travis CI builds, it is automatically configured from 'TRAVIS_BRANCH' environment variable.\n" +
+                "If you are trying to run this task outside Travis, you can export the environment variable.\n" +
+                "Alternatively, you can set the task's 'rev' property explicitly."));
 
         //update release needed task based on Travis' env variables
         String pr = System.getenv("TRAVIS_PULL_REQUEST");
@@ -68,11 +66,15 @@ public class TravisPlugin implements Plugin<Project> {
         final boolean isPullRequest = pr != null && !pr.trim().isEmpty() && !pr.equals("false");
         LOG.info("Pull request build: {}", isPullRequest);
 
-        project.getTasks().withType(ReleaseNeededTask.class, new Action<ReleaseNeededTask>() {
-            public void execute(ReleaseNeededTask t) {
-                t.setCommitMessage(System.getenv("TRAVIS_COMMIT_MESSAGE"));
-                t.setPullRequest(isPullRequest);
-            }
+        String travisJobUrl = generateTravisBuildUrl(conf);
+        String commitMessage = System.getenv("TRAVIS_COMMIT_MESSAGE") + ". CI job: " + travisJobUrl;
+        project.getTasks().withType(ReleaseNeededTask.class, t -> {
+            t.setCommitMessage(commitMessage);
+            t.setPullRequest(isPullRequest);
         });
+    }
+
+    private String generateTravisBuildUrl(ShipkitConfiguration conf) {
+        return "https://travis-ci.org/" + conf.getGitHub().getRepository()+ "/builds/" + System.getenv("TRAVIS_BUILD_NUMBER");
     }
 }
