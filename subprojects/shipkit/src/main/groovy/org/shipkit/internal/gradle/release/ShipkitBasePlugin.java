3d68874d518542ca8b8babbf9648d127dc9a74a9
Szczepan Faber <szczepiq@gmail.com>
2018-03-22 09:38:58 -0700

    Added new plugin for linkedin/play-parseq use case

--- subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/release/ShipkitBasePlugin.java
+++ subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/release/ShipkitBasePlugin.java
@@ -3,11 +3,14 @@ package org.shipkit.internal.gradle.release;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.shipkit.internal.gradle.bintray.BintrayReleasePlugin;
+import org.shipkit.internal.gradle.bintray.ShipkitBintrayPlugin;
 
 /**
  * Continuous delivery for any project that wants to publish to Bintray with Travis.
- * Intended for root project of your Gradle project.
- * Originally created to enable Shipkit in project (<a href="https://github.com/linkedin/play-parseq">https://github.com/linkedin/play-parseq</a>).
+ * Intended for root project of your multi-project Gradle build.
+ * In order for this plugin to work, please apply {@link ShipkitBintrayPlugin} to
+ * every Gradle subproject that you want to publish to Bintray.
+ *
  * <p>
  * Applies following plugins:
  *
