d05135ffa78fe728967c77a54e000e0d32271a1d
Szczepan Faber <szczepiq@gmail.com>
2018-04-13 09:31:51 -0700

    Added unit tests

--- subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/snapshot/LocalSnapshotPlugin.java
+++ subprojects/shipkit/src/main/groovy/org/shipkit/internal/gradle/snapshot/LocalSnapshotPlugin.java
@@ -3,9 +3,12 @@ package org.shipkit.internal.gradle.snapshot;
 import org.gradle.StartParameter;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
 import org.shipkit.internal.gradle.util.TaskMaker;
+
+import java.util.List;
 
 import static org.shipkit.internal.gradle.util.Specs.withName;
 
@@ -24,22 +27,26 @@ public class LocalSnapshotPlugin implements Plugin<Project> {
 
     public static final String SNAPSHOT_TASK = "snapshot";
     private final static Logger LOG = Logging.getLogger(LocalSnapshotPlugin.class);
-    private boolean isSnapshot;
+    private Boolean isSnapshot;
 
     @Override
     public void apply(Project project) {
-        this.isSnapshot = project.getGradle().getStartParameter().getTaskNames().contains(SNAPSHOT_TASK);
-
-        TaskMaker.task(project, SNAPSHOT_TASK, t -> {
+        Task snapshotTask = TaskMaker.task(project, SNAPSHOT_TASK, t -> {
             t.setDescription("Depends on specific tasks that create local snapshot files.");
-
-            if (isSnapshot) {
-                project.getTasks().matching(withName("javadoc", "groovydoc")).all(doc -> {
-                    LOG.info("{} - disabled to speed up the 'snapshot' build", t.getPath());
-                    doc.setEnabled(false);
-                });
-            }
         });
+
+        this.isSnapshot = configureTask(snapshotTask, project.getGradle().getStartParameter().getTaskNames());
+    }
+
+    static boolean configureTask(Task snapshotTask, List<String> taskNames) {
+        boolean isSnapshot = taskNames.contains(SNAPSHOT_TASK);
+        if (isSnapshot) {
+            snapshotTask.getProject().getTasks().matching(withName("javadoc", "groovydoc")).all(doc -> {
+                LOG.info("{} - disabled to speed up the 'snapshot' build", snapshotTask.getPath());
+                doc.setEnabled(false);
+            });
+        }
+        return isSnapshot;
     }
 
     public boolean isSnapshot() {
